ce59d9b1b8298156f39f8ab5158a0ed6d6e1fe80
Hayden <mrhaydendp@gmail.com>
2021-08-25 16:11:04 -0700

    Update RemoveEdge.ps1

--- RemoveEdge.ps1
+++ RemoveEdge.ps1
@@ -1,5 +1,5 @@
 # Removes Edge installer
-Remove-Item 'C:\Program Files (x86)\Microsoft\Edge\Application\*\Installer\setup.exe'
+'C:\Program Files (x86)\Microsoft\Edge\Application\*\Installer\setup.exe' --uninstall --system-level --verbose-logging --force-uninstall
 
 # Adds registry file that tells Windows you have the old Edge browser
 Invoke-WebRequest -Uri https://christitus.com/files/stopedge.zip -OutFile ./stopedge.zip
