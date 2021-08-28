726336ce06216bac167b4632633f45b82f8d0196
Hayden <mrhaydendp@gmail.com>
2021-08-28 08:26:11 -0700

    Update RemoveEdge.ps1
    Changed to ~ so script works regardless of where you start from.

--- RemoveEdge.ps1
+++ RemoveEdge.ps1
@@ -1,18 +1,15 @@
-# Go to home directory
-cd ~
-
 # Removes Edge installer
 & 'C:\Program Files (x86)\Microsoft\Edge\Application\*\Installer\setup.exe' --uninstall --system-level --verbose-logging --force-uninstall
 
 # Adds registry file that tells Windows you have the old Edge browser
-Start-BitsTransfer -Source https://christitus.com/files/stopedge.zip -Destination ./
+Start-BitsTransfer https://christitus.com/files/stopedge.zip ~\stopedge.zip
 
 # Unzip and run .reg file
-Expand-Archive .\stopedge.zip .\
-.\stopedge.reg
+Expand-Archive ~\stopedge.zip ~
+~\stopedge.reg
 
 # Wait 10 seconds
 Start-Sleep 10
 
 # Cleanup
-Remove-Item ./stopedge*
+Remove-Item ~\stopedge*
