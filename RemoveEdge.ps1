d5eb6e3d5eccba6cc76071bcdffe13b856d32772
Hayden Plumley <mrhaydendp@gmail.com>
2021-10-22 16:06:48 -0700

    Update RemoveEdge.ps1
    - Fixed issue where you couldn't delete Edge if you have multiple versions installed

--- RemoveEdge.ps1
+++ RemoveEdge.ps1
@@ -1,5 +1,6 @@
-# Remove Edge installer
-& 'C:\Program Files (x86)\Microsoft\Edge\Application\*\Installer\setup.exe' --uninstall --system-level --verbose-logging --force-uninstall
+# Delete latest version of Edge and all of its subfolders
+$edge = @(Get-ChildItem 'C:\Program Files (x86)\Microsoft\Edge\Application\*\Installer\setup.exe')
+& $edge[0] --uninstall --system-level --verbose-logging --force-uninstall
 
 # Create registry file that tells Windows you have the old Edge browser
 Set-Location HKLM:\SOFTWARE\Microsoft\
