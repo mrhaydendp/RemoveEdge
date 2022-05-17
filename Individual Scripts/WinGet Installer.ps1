2c00e5beaa2705ba5de7a79cac6b02220d1a2271
Hayden Plumley <mrhaydendp@gmail.com>
2022-05-17 13:57:09 -0700

    Simplified and Rewrote Script
    - New installation method uses the Microsoft Store for simplicity and allows for automatic updates

--- Individual Scripts/WinGet Installer.ps1
+++ Individual Scripts/WinGet Installer.ps1
@@ -1,13 +1,5 @@
-# Check if WinGet is Installed, if not Run Script
-if (!(Test-Path "~\AppData\Local\Microsoft\WindowsApps\winget.exe")){
-    # Get Architecture Type & Install Latest Version of VCLibs From MS Docs
-    $arch = "x64"
-    if ((Get-WmiObject -Class Win32_ComputerSystem).SystemType -match '(x86)'){
-        $arch = "x86"
-    }
-    $vclibs = (Invoke-WebRequest -UseBasicParsing -URI "https://docs.microsoft.com/en-us/troubleshoot/developer/visualstudio/cpp/libraries/c-runtime-packages-desktop-bridge").Links.Href | Select-String "Microsoft.VCLibs.$arch"
-    Add-AppxPackage -Path "$vclibs"
-    # Grab Latest Stable Release of WinGet from GitHub
-    $latest = (Invoke-WebRequest -UseBasicParsing -URI "https://github.com/microsoft/winget-cli/releases/latest").Links.Href | Select-String ".msixbundle"
-    Add-AppxPackage -Path "https://github.com$latest"
+# If Winget isn't Installed, Open App Installer Store Page to Install
+if (!(Get-AppxPackage -name "Microsoft.DesktopAppInstaller")) {
+    Start-Process "ms-windows-store://pdp/?ProductId=9NBLGGH4NNS1"
+    $nid = (Get-Process WinStore.App).Id; Wait-Process -Id $nid
 }
