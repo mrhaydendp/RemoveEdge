cbe4ba8a4d786776a242810a77584d509e6c16bd
Hayden Plumley <mrhaydendp@gmail.com>
2022-06-29 12:54:08 -0700

    Updated to work on newest Windows update

--- RemoveEdge.ps1
+++ RemoveEdge.ps1
@@ -1,7 +1,8 @@
 # Delete latest version of Edge and all of its subfolders
 $edge = (Get-ChildItem 'C:\Program Files (x86)\Microsoft\Edge\Application\*\Installer\setup.exe')
 & $edge[0] --uninstall --system-level --verbose-logging --force-uninstall
+& 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe' --uninstall --system-level --verbose-logging --force-uninstall
 
-# Create registry file that tells Windows you have the old Edge browser
+# Create registry file that tells Windows you have the old Edge browser (disables updates)
 New-Item -Path "HKLM:\SOFTWARE\Microsoft\" -Name "EdgeUpdate" -Force
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\EdgeUpdate\" -Name 'DoNotUpdateToEdgeWithChromium' -Force -Type DWORD -Value 1
