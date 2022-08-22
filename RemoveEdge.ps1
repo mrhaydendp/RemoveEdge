745c35cdcac8702fd685c00364bb9d008e5e7e53
Hayden Plumley <mrhaydendp@gmail.com>
2022-08-22 10:03:52 -0700

    Update RemoveEdge.ps1
    - Piped `Select -First 1` command for edge variable so array is not needed
    - Removed extra system-level uninstall as it was unneeded
    - Updated code comments

--- RemoveEdge.ps1
+++ RemoveEdge.ps1
@@ -1,8 +1,7 @@
-# Delete latest version of Edge and all of its subfolders
-$edge = (Get-ChildItem 'C:\Program Files (x86)\Microsoft\Edge\Application\*\Installer\setup.exe')
-& $edge[0] --uninstall --system-level --verbose-logging --force-uninstall
-& 'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe' --uninstall --system-level --verbose-logging --force-uninstall
+# Delete Edge setup.exe and all of its subfolders
+$edge = (Get-ChildItem 'C:\Program Files (x86)\Microsoft\Edge\Application\*\Installer\setup.exe' | Select -First 1)
+& $edge --uninstall --system-level --verbose-logging --force-uninstall
 
-# Create registry file that tells Windows you have the old Edge browser (disables updates)
+# Create registry file that tells Windows you have the old non-Chromium Edge browser (disables updates)
 New-Item -Path "HKLM:\SOFTWARE\Microsoft\" -Name "EdgeUpdate" -Force
 New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\EdgeUpdate\" -Name 'DoNotUpdateToEdgeWithChromium' -Force -Type DWORD -Value 1
