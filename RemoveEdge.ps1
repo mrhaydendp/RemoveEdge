6dd6987bc3adcacc38a6a786a04734611065a38b
Hayden Plumley <mrhaydendp@gmail.com>
2022-12-06 11:24:35 -0800

    Fixed Errors and Edge Cases
    - Fixed error thrown if Edge isn't running
    - Added 2 second buffer to make sure Edge is closed before deleting application directory

--- RemoveEdge.ps1
+++ RemoveEdge.ps1
@@ -1,5 +1,6 @@
 # Force close Edge & delete application directory
-Get-Process msedge | Stop-Process
+Get-Process msedge -ErrorAction SilentlyContinue | Stop-Process
+Start-Sleep 2
 Remove-Item 'C:\Program Files (x86)\Microsoft\Edge\' -Recurse -Force
 
 # Create registry file that tells Windows you have the old non-Chromium Edge browser (disables updates)
