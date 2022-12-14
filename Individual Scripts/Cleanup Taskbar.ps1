bc0ec48bddde0ec500c9f972ba2143d5fde372cd
Hayden Plumley <mrhaydendp@gmail.com>
2022-12-14 15:36:08 -0800

    Added Comment & Removal of Search Bar
    - Added comment to describe what script does
    - Cleaned up method of going through array
    - Moved `-Force` flags to end of commands to make more sense
    - Added removal of new search bar in taskbar

--- Individual Scripts/Cleanup Taskbar.ps1
+++ Individual Scripts/Cleanup Taskbar.ps1
@@ -1,6 +1,7 @@
-$icons = @("TaskbarMn","TaskbarDa","ShowTaskViewButton","TaskbarAl")
-foreach ($icon in $icons){
-    New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\" -Name "$icon" -Force -Type DWORD -Value 0
+# Center Taskbar and Hide Icons & Disable Search in Start  
+@("TaskbarMn","TaskbarDa","ShowTaskViewButton","TaskbarAl") | % {
+    New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\" -Name "$_" -Type DWORD -Value 0 -Force
 }
+New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search\" -Name "SearchboxTaskbarMode" -Type DWORD -Value 0 -Force
 New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\" -Name "Explorer" -Force
-New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer\" -Name "DisableSearchBoxSuggestions" -Force -Type DWORD -Value 1
+New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer\" -Name "DisableSearchBoxSuggestions" -Type DWORD -Value 1 -Force
