057722b39df9e6f39fbb156714ff19159405573d
Hayden Plumley <mrhaydendp@gmail.com>
2022-12-14 16:07:35 -0800

    Updated Methods
    - Updated Appx installation method and added 2 more for different systems / use cases

--- Individual Scripts/Winget Installer.ps1
+++ Individual Scripts/Winget Installer.ps1
@@ -1,8 +1,22 @@
-# Determine System Architecture
-$arch = ([IntPtr]::Size * 8)
-if ("$arch" -eq 32) {
-$arch = 86
+# Manual Appinstaller Method
+if (!(Test-Path "$HOME\AppData\Local\Microsoft\WindowsApps\winget.exe")){
+    Start-BitsTransfer "https://aka.ms/getwinget" getwinget.msixbundle; .\getwinget.msixbundle
+    Get-Process AppInstaller | Wait-Process
+    Write-Host "Successfully Installed Winget"
 }
 
-# Install VCLibs & Winget using Microsoft Shortlinks 
-Add-AppxPackage -DependencyPath "https://aka.ms/Microsoft.VCLibs.x$arch.14.00.Desktop.appx" -Path "https://aka.ms/getwinget" -ForceUpdateFromAnyVersion	
+# Windows Store Method
+if (!(Test-Path "$HOME\AppData\Local\Microsoft\WindowsApps\winget.exe")){
+    Start-Process "ms-windows-store://pdp/?ProductId=9NBLGGH4NNS1"
+    Get-Process WinStore.App | Wait-Process
+    Write-Host "Successfully Installed Winget"
+}
+
+# Manual Add-Appx Method
+if (!(Test-Path "$HOME\AppData\Local\Microsoft\WindowsApps\winget.exe")){
+    $arch = "64"
+    if (([IntPtr]::Size * 8) -eq 32){
+        $arch = "86"
+    }
+    Add-AppxPackage -DependencyPath "https://aka.ms/Microsoft.VCLibs.x$arch.14.00.Desktop.appx" -Path "https://aka.ms/getwinget" -ForceUpdateFromAnyVersion
+}
