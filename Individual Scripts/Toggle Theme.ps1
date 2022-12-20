bd0886522016a64db15f30e2da704fb11baaea14
Hayden Plumley <mrhaydendp@gmail.com>
2022-12-20 11:39:47 -0800

    Update Toggle Theme.ps1
    - Simplified theme change by setting $value instead of using if else

--- Individual Scripts/Toggle Theme.ps1
+++ Individual Scripts/Toggle Theme.ps1
@@ -1,6 +1,6 @@
 # If "SystemUsesLightTheme" is True, Switch to Dark Theme Using Registry Keys Else, Switch to Light Theme
+$value = "1"
 if (Get-ItemPropertyValue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme"){
-    @("AppsUseLightTheme","SystemUsesLightTheme") | % { New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "$_" -Type DWORD -Value 0 -Force }   
-} else {
-    @("AppsUseLightTheme","SystemUsesLightTheme") | % { New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "$_" -Type DWORD -Value 1 -Force }   
+    $value = "0"
 }
+@("AppsUseLightTheme","SystemUsesLightTheme") | % { New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "$_" -Type DWORD -Value "$value" -Force }   
