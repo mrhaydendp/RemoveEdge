61c6809eef14828040deb98edc0bc422ae5ba4d9
Hayden Plumley <mrhaydendp@gmail.com>
2022-12-20 12:25:32 -0800

    Update RemoveEdge.ps1
    - Cleanup formatting
    - Added more Edge folders to remove

--- RemoveEdge.ps1
+++ RemoveEdge.ps1
@@ -1,8 +1,9 @@
 # Force close Edge & delete application directory
 Get-Process msedge -ErrorAction SilentlyContinue | Stop-Process
 Start-Sleep 2
-Remove-Item 'C:\Program Files (x86)\Microsoft\Edge\' -Recurse -Force
+Remove-Item "C:\Program Files (x86)\Microsoft\Edge\" -Recurse -Force
+Get-Item "$HOME\AppData\Local\Packages\Microsoft.MicrosoftEdge*" | % { Remove-Item "$_" -Recurse -Force }
 
 # Create registry file that tells Windows you have the old non-Chromium Edge browser (disables updates)
 New-Item -Path "HKLM:\SOFTWARE\Microsoft\" -Name "EdgeUpdate" -Force
-New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\EdgeUpdate\" -Name 'DoNotUpdateToEdgeWithChromium' -Force -Type DWORD -Value 1
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\EdgeUpdate\" -Name "DoNotUpdateToEdgeWithChromium" -Type DWORD -Value 1 -Force
