ef1ea3fe689782318999851b5664ec78b153147d
Hayden Plumley <mrhaydendp@gmail.com>
2023-04-01 16:46:57 -0700

    Update Toggle Theme.ps1
    - Formatting fixes
    - Now restarts Explorer for theme to apply

--- Individual Scripts/Toggle Theme.ps1
+++ Individual Scripts/Toggle Theme.ps1
@@ -1,6 +1,11 @@
-# If "SystemUsesLightTheme" is True, Switch to Dark Theme Using Registry Keys Else, Switch to Light Theme
+# If "SystemUsesLightTheme" is True Switch to Dark Theme Using Registry Keys Else, Switch to Light Theme
 $value = "1"
 if (Get-ItemPropertyValue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme"){
     $value = "0"
 }
-@("AppsUseLightTheme","SystemUsesLightTheme") | % { New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "$_" -Type DWORD -Value "$value" -Force }   
+@("AppsUseLightTheme","SystemUsesLightTheme") | % {
+    New-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "$_" -Type DWORD -Value "$value" -Force
+}
+
+# Restart Explorer to Apply Changes
+Get-Process Explorer | Stop-Process
