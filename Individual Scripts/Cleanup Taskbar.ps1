6473c5bf828b2141d82e7008c96d92957e38ca01
Hayden Plumley <mrhaydendp@gmail.com>
2023-07-29 14:50:24 -0700

    Update Cleanup Taskbar.ps1
    - Updated comments
    - Better organized code
    - Script will now restart Explorer to apply changes instead of waiting for reboot

--- Individual Scripts/Cleanup Taskbar.ps1
+++ Individual Scripts/Cleanup Taskbar.ps1
@@ -1,7 +1,12 @@
-# Center Taskbar and Hide Icons & Disable Search in Start  
+# Center taskbar & remove MS items
 @("TaskbarMn","TaskbarDa","ShowTaskViewButton","TaskbarAl") | % {
     New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\" -Name "$_" -Type DWORD -Value 0 -Force
 }
+
+# Disable search in start menu
 New-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search\" -Name "SearchboxTaskbarMode" -Type DWORD -Value 0 -Force
 New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\" -Name "Explorer" -Force
 New-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer\" -Name "DisableSearchBoxSuggestions" -Type DWORD -Value 1 -Force
+
+# Restart Explorer to apply changes
+Get-Process explorer | Stop-Process
