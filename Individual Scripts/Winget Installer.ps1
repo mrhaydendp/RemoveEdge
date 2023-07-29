12d71d95cdd984a6cf95dd46018c2205c645e91b
Hayden Plumley <mrhaydendp@gmail.com>
2023-07-29 15:19:46 -0700

    Update Winget Installer.ps1
    - Added Winget check at the start of the script
    - Added terminal ui to choose installation method
    - Added output to terminal to explain installation method
    - Added status check

--- Individual Scripts/Winget Installer.ps1
+++ Individual Scripts/Winget Installer.ps1
@@ -1,22 +1,34 @@
-# Manual Appinstaller Method
-if (!(Test-Path "$HOME\AppData\Local\Microsoft\WindowsApps\winget.exe")){
+# Check if Winget is already installed
+if (Test-Path "$HOME\AppData\Local\Microsoft\WindowsApps\winget.exe"){
+    Write-Host "Winget Already Installed, Exiting..."
+    pause; exit
+}
+
+# Select installer method
+$option = Read-Host "Winget Installation Methods:`n1. Appinstaller`n2. Windows Store`n3. Add-Appx`nOptions (1,2,3)"
+
+# Installation methods
+if ("$option" -eq "1"){
+    Write-Host "Installing 'getwinget.msixbundle' from Official Microsoft Link & Running with Appinstaller"
     Start-BitsTransfer "https://aka.ms/getwinget" getwinget.msixbundle; .\getwinget.msixbundle
     Get-Process AppInstaller | Wait-Process
-    Write-Host "Successfully Installed Winget"
-}
-
-# Windows Store Method
-if (!(Test-Path "$HOME\AppData\Local\Microsoft\WindowsApps\winget.exe")){
+} elseif ("$option" -eq "2"){
+    Write-Host "Opening Store Link..."
     Start-Process "ms-windows-store://pdp/?ProductId=9NBLGGH4NNS1"
     Get-Process WinStore.App | Wait-Process
-    Write-Host "Successfully Installed Winget"
-}
-
-# Manual Add-Appx Method
-if (!(Test-Path "$HOME\AppData\Local\Microsoft\WindowsApps\winget.exe")){
+} elseif ("$option" -eq "3"){
+    Write-Host "Checking Architecture"
     $arch = "64"
     if (([IntPtr]::Size * 8) -eq 32){
         $arch = "86"
     }
+    Write-Host "Stream-installing Winget & VCLibs"
     Add-AppxPackage -DependencyPath "https://aka.ms/Microsoft.VCLibs.x$arch.14.00.Desktop.appx" -Path "https://aka.ms/getwinget" -ForceUpdateFromAnyVersion
 }
+
+# Check if Winget status
+$status = "Failed to Install Winget"
+if (Test-Path "$HOME\AppData\Local\Microsoft\WindowsApps\winget.exe"){
+    $status = "Winget Successfully Installed"
+}
+Write-Host "$status"
