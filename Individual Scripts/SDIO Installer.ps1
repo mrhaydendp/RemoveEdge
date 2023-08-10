373baaa173ea10515f637f8cadbf30fea4822ca4
Hayden Plumley <mrhaydendp@gmail.com>
2023-08-10 11:26:46 -0700

    Update SDIO Installer.ps1
    - Swapped to a faster method of finding latest SDIO version
    - Added terminal output to script to explain what's happening
    - Added checks to make sure you don't download an empty file
    - Added success and error messages
    - Added comment explaining code

--- Individual Scripts/SDIO Installer.ps1
+++ Individual Scripts/SDIO Installer.ps1
@@ -1,1 +1,12 @@
-(Invoke-WebRequest "https://www.glenn.delahoy.com/snappy-driver-installer-origin/").Links.Href | Select-String "SDIO_(.*?)[0-9].zip" | % { Start-BitsTransfer "https://www.glenn.delahoy.com$_" -Destination "$HOME\Desktop" }
+# Add latest SDIO version to download link & download to $HOME\Desktop
+Write-Host "Finding Latest SDIO Version From: 'https://www.glenn.delahoy.com/snappy-driver-installer-origin'"
+$latest = (Invoke-RestMethod "https://www.glenn.delahoy.com/snappy-driver-installer-origin/" | Select-String "SDIO_(.*?)[0-9].zip").Matches.Value
+if (!("$latest")){
+    Write-Host "Error: Could not Find Latest SDIO Version`nExiting..."
+    exit
+}
+Write-Host "Found: $latest"
+Start-BitsTransfer -Source "https://www.glenn.delahoy.com/snappy-driver-installer-origin/downloads/sdio/$latest" -Destination "$HOME\Desktop"
+if (Test-Path "$HOME\Desktop\$latest"){
+    Write-Host "Successfully Installed to: '$HOME\Desktop\$latest'"
+}
